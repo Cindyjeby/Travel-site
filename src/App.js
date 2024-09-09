4b7ed53241b86c1e7543a1ff71bb8694cf92f646
cindyjeby <cindyjeby@gmail.com>
2024-09-09 13:16:27 +0300

    created a hero section for the home page, learnied hoe to add video and added css for style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
-        <Route path="/" element={<Navbar />}>
+        <Route exact path="/" element={<Home />}>
         </Route>
       </Routes>
     </BrowserRouter>
