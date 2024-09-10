32dfd6d0ef9a0f67628e46835a503f67e8219e70
cindyjeby <cindyjeby@gmail.com>
2024-09-10 13:44:47 +0300

    made the footer and made some changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home'
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />}>
-        </Route>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
